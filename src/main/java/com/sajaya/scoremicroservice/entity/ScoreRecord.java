1d829eb0130d2176509973e0762306b65095044f
Maryam sadat <maryam.s01382@gmail.com>
2024-07-27 16:01:19 +0330

    add ScoreRecord entity

---
+++ src/main/java/com/sajaya/scoremicroservice/entity/ScoreRecord.java
@@ -0,0 +1,26 @@
+package com.sajaya.scoremicroservice.entity;
+
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Size;
+
+@Entity
+@Table(name = "scores")
+public class ScoreRecord {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long recordId;
+
+    @ManyToOne
+    @JoinColumn(name = "subject_id", nullable = false)
+    private Subject subject;
+
+    @ManyToOne
+    @JoinColumn(name = "student_id", nullable = false)
+    private Student student;
+
+    @Column(name = "score")
+    @Size(min = 0, max = 20, message = "The score should be between 0 and 20")
+    private double score;
+
+}
